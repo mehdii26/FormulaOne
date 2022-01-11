c453cf30ec01d32d3744572d5598eea8733b5bee
Anis Mehenni <ga_mehenni@esi.dz>
2022-01-11 12:22:16 +0100

    feature US011 and stepDef USO11

--- src/stepDefinitions/StepDefs_US011_ChoisirPneu.java
+++ src/stepDefinitions/StepDefs_US011_ChoisirPneu.java
@@ -10,44 +10,43 @@ import strategieCourse.Pneu;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefs_US011_ChoisirPneu {
+
     private Essai essai1;
     private Essai essai2;
     private Pilote pilote;
     private Pneu pneu1;
     private Pneu pneu2;
 
-    @Given("l'essai numero {int} avec {String}")
-    public void instancierEssai1(int numEssai, String typePneu){
-        this.pilote = new Pilote();
-        this.essai1 = new Essai();
-        this.pneu1 = new Pneu();
-        this.essai1.setNumEssai(numEssai);
-        this.essai1.setPilote(this.pilote);
-        this.pneu1.setNom(typePneu);
-        this.essai2.setPneu(this.pneu2);
-    }
+    @Given("l'essai numero {int} avec {string}")
+            public void initierEssai1(int arg0, String arg1){
+            this.pilote = new Pilote();
+            this.essai1 = new Essai();
+            this.pneu1 = new Pneu();
+            this.pneu1.setNom(arg1);
+            this.essai1.setNumEssai(arg0);
+            this.essai1.setPilote(this.pilote);
+            this.essai1.setPneu(this.pneu1);
+        };
 
-    @Given("et l'essai numero {int} avec {String}")
-    public void instancierEssai2(int numEssai, String typePneu){
-        this.essai2 = new Essai();
-        this.pneu2 = new Pneu();
-        this.essai2.setNumEssai(numEssai);
-        this.essai2.setPilote(this.pilote);
-        this.pneu2.setNom(typePneu);
-        this.essai2.setPneu(this.pneu2);
-    }
+    @Given("et l'essai numero {int} avec {string}")
+        public void initierEssai2 (Integer arg0, String arg1){
+            this.essai2 = new Essai();
+            this.pneu2 = new Pneu();
+            this.essai2.setNumEssai(arg0);
+            this.essai2.setPilote(this.pilote);
+            this.pneu2.setNom(arg1);
+            this.essai2.setPneu(this.pneu2);
+        };
 
-    @When("{int} de essai1 est inferieur a {int} de essai2")
-    public void setEssaiTimes(int durre1, int durre2){
-        this.essai1.setDuree(durre1);
-        this.essai2.setDuree(durre2);
-    }
+        @When("^(\\d+) de essai1 est inferieur a (\\d+) de essai2$")
+        public void comparerEssai1EtEssai2(Integer arg0, Integer arg1)  {
+            this.essai1.setDuree(arg0);
+            this.essai2.setDuree(arg1);
+        };
 
-    @Then("le {String} est selectionne automatiquement")
-    public void verifierPneuChoisi(String pneuType){
-        assertEquals(pneuType, this.essai1.getPneu().getNom());
-    }
+        @Then("le {string} est selectionne automatiquement")
+        public void choisirStrategie(String arg0) {
+            assertEquals(arg0, this.essai1.getPneu().getNom());
+        };
 
 }
-
-
